0456d1dc41fd8f4746c767389ed017bc3c1bf5fd
Zukkari <stanislav.myshkovski@gmail.com>
2019-12-03 23:00:57 +0200

    Rework RepoFileParser to be more functional

--- src/main/scala/io/github/zukkari/actors/RepoFileParser.scala
+++ src/main/scala/io/github/zukkari/actors/RepoFileParser.scala
@@ -2,12 +2,13 @@ package io.github.zukkari.actors
 
 import java.io.File
 
-import akka.actor.{Actor, Props}
+import akka.actor.{Actor, ActorSelection, Props}
 import akka.event.Logging
-import io.github.zukkari.parser.ProjectFileParser
+import cats.effect.{IO, Resource}
+import io.github.zukkari.parser.{Project, ProjectFileParser}
 
+import scala.annotation.tailrec
 import scala.io.Source
-import scala.util.Using
 
 object RepoFileParser {
   def props(parser: ProjectFileParser): Props = Props(new RepoFileParser(parser))
@@ -20,24 +21,54 @@ class RepoFileParser(private val parser: ProjectFileParser) extends Actor {
     case repoFile: File =>
       log.info(s"Received file: ${repoFile.getAbsolutePath}")
 
-      val router = context.actorSelection("../gitSourceRouter")
-
-      Using(Source.fromFile(repoFile))(_.getLines().toSeq)
-        .map(seq => seq.map(parser.parse))
-        .fold(
-          err => {
+      implicit val router: ActorSelection = context.actorSelection("../gitSourceRouter")
+      parse(repoFile)
+        .runAsync {
+          case Left(err) => IO {
             log.error("Error when parsing input file", err)
-            Seq.empty
-          },
-          identity
-        )
-        .foreach({
-          case Some(project) => router ! project
-          case _ =>
-        })
+          }
+          case Right(c) => IO {
+            log.info(s"Successfully parsed $c projects")
+          }
+        }
     case _ =>
       log.error("Received unknown message", _)
   }
 
+  def parse(f: File)(implicit router: ActorSelection): IO[Int] =
+    lines(f)
+      .map(transform)
+      .use(send)
+
+  def send(seq: Seq[Project])(implicit router: ActorSelection): IO[Int] = {
+    @tailrec
+    def _send(seq: Seq[Project], acc: Int): IO[Int] = {
+      seq match {
+        case x :: xs =>
+          router ! x
+          _send(xs, acc + 1)
+        case Nil =>
+          IO.pure(acc)
+      }
+    }
+
+    _send(seq, 0)
+  }
+
+  def transform(seq: Seq[String]): Seq[Project] =
+    for {
+      line <- seq
+      parsed <- parser.parse(line)
+    } yield parsed
+
+
+  def lines(f: File): Resource[IO, Seq[String]] = {
+    val io = IO {
+      Source.fromFile(f)
+    }
+
+    Resource.fromAutoCloseable(io)
+      .map(_.getLines().toSeq)
+  }
 }
 
